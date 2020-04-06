806d4b92c52c4265bdcb29c664d115a453909af8
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-04-06 18:54:22 -0500

    HomPage Finished, Router pending

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+import BodyHome from './components/BodyHome';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-fluid">
+        <Navbar></Navbar>        
+      </div>
+      <BodyHome></BodyHome>
+      <div className="container">
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
---
+++ front/src/components/BodyHome.js
@@ -0,0 +1,67 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+
+const BodyHome = () => {
+  return (
+    <div className="container-fluid">
+      <h1>Covid 19 - Symtoms</h1>
+      <div className="paragraph">
+        Covid-19 Symtoms es una aplicación cuyo objetivo es que cualquier
+        persona que pueda presentar sintomas de COVID-19 puedan reportarlos e ir
+        llevando un control de todo lo relacionado con estos. Lo único que hay
+        que hacer es registrate y en la aplicación podrás ir colocando todo lo
+        relacionado con el coronavirus.
+      </div>
+      <h2>Sintomas del coronavirus</h2>
+      <div className="paragraph">
+      Aunque hay pacientes con COVID-19 asintomáticas, la gran mayoría presentan una sintomatología
+      común. Entre los sintomas más frecuentes se encuentran:
+        <div className="containerImagenes">
+          <div className="row">
+            <div className="col">
+              <img src="./fever.png" alt="..." class="img-symtoms"></img>
+              <div class="overlay">
+                <div class="text">Fiebre</div>
+              </div>
+            </div>
+            <div className="col">
+              <img src="./tos.svg" alt="..." class="img-symtoms"></img>
+              <div class="overlay">
+                <div class="text">Tos</div>
+              </div>
+            </div>
+            <div className="col">
+              <img src="./respirar.svg" alt="..." class="img-symtoms "></img>
+              <div class="overlay">
+                <div class="text">Ahogo</div>
+              </div>
+            </div>            
+          </div>
+          <div className="row">
+            <div className="col">
+              <img src="./dolor.svg" alt="..." class="img-symtoms"></img>
+              <div class="overlay">
+                <div class="text">Dolor</div>
+              </div>
+            </div>
+            <div className="col">
+              <img src="./congestion.svg" alt="..." class="img-symtoms"></img>
+              <div class="overlay">
+                <div class="text">Congestion</div>
+              </div>
+            </div>
+            <div className="col">
+              <img src="./fatiga.svg" alt="..." class="img-symtoms "></img>
+              <div class="overlay">
+                <div class="text">Fatiga</div>
+              </div>
+            </div>            
+          </div>
+        </div>  
+        Si tienes uno de estos sintomas comienza a registrarlos  <a className="red" href="#">Aquí</a>    
+      </div>
+    </div>
+  );
+};
+
+export default BodyHome;
