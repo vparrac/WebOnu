723c113efbe47b42287559fc7dd9eeb8f8fd6530
whatevercamps <dj.bautista10@uniandes.edu.co>
2020-04-14 12:02:44 -0500

    Update app.js
    Syntax required fix

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 configurePassport(app);
-app.use('/', indexRouter);
-app.use('/registrar', registrarRouter);
+app.use("/", indexRouter);
+app.use("/registrar", registrarRouter);
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
@@ -54,21 +54,5 @@ app.use(function (req, res, next) {
 });
 app.use("/", indexRouter);
 app.use("/", authenticationRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
